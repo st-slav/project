afb1dfd1249ddcfdb1b9aca1c150be52b0609367
Stas Mamontov <smamontov@ibs.ru>
2021-06-07 16:17:34 +0500

    init webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+
+import { test } from "./test"
+
 class Paper {
   constructor(x) {
     this.x = x
   }
   get paper() { return 23 };
-  async test() {
-    const { test } = await import("./test")
+  test() {
     return test()
   }
 }
+
+console.log(new Paper(12).test())
